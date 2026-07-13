fb85ce4a70bc2ccf1751121f95ffaa5edf81aa7f
aksharabiju <akshara.biju@sociusmail.com>
2026-07-13 19:22:13 +0530

    [UPD]

--- erzberger_customizations/models/__init__.py
+++ erzberger_customizations/models/__init__.py
@@ -3,4 +3,3 @@
 from . import sale_order
 from . import purchase_order
 from . import print_label_layout
-
--- erzberger_customizations/models/sale_order.py
+++ erzberger_customizations/models/sale_order.py
@@ -8,3 +8,13 @@ class SaleOrder(models.Model):
         string="Job / Project Reference",
         help="Reference of the print job or project."
     )
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _purchase_service_prepare_order_values(self, supplierinfo):
+        values = super()._purchase_service_prepare_order_values(supplierinfo)
+
+        values["job_reference"] = self.order_id.job_reference
+
+        return values
